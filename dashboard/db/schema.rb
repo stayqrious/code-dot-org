7ddff1fa44d38bd2f2a92aa08208d6b272245f37
David Bailey <davidsbailey@users.noreply.github.com>
2020-05-28 16:06:30 -0700

    add owner to script

--- dashboard/db/schema.rb
+++ dashboard/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200528200139) do
+ActiveRecord::Schema.define(version: 20200528230412) do
 
   create_table "activities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.integer  "user_id"
@@ -1327,6 +1327,7 @@ ActiveRecord::Schema.define(version: 20200528200139) do
     t.text     "properties",      limit: 65535
     t.string   "new_name"
     t.string   "family_name"
+    t.integer  "owner_id"
     t.index ["family_name"], name: "index_scripts_on_family_name", using: :btree
     t.index ["name"], name: "index_scripts_on_name", unique: true, using: :btree
     t.index ["new_name"], name: "index_scripts_on_new_name", unique: true, using: :btree
