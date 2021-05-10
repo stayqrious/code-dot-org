a4744f5cdc52765b42dff97e68162096f33912da
Siddharth Prajosh <65843976+siddharth-sq@users.noreply.github.com>
2021-05-10 14:31:27 +0530

    Popup design (#3)
    * add assets
    
    * update message
    
    * baby img
    
    * update design
    
    * fix height
    
    * make icon baby the default
    
    Co-authored-by: Arun Prabhakar <arun.pbk@stayq.com>

--- apps/src/feedback.js
+++ apps/src/feedback.js
@@ -177,6 +177,7 @@ FeedbackUtils.prototype.displayFeedback = function(
   }
 
   feedback.className += canContinue ? ' win-feedback' : ' failure-feedback';
+  feedback.className += ' sq-win-feedback'
 
   var finalLevel =
     options.response && options.response.message === 'no more levels';
@@ -674,6 +675,7 @@ FeedbackUtils.prototype.getNumCountableBlocks = function() {
 FeedbackUtils.prototype.getFeedbackButtons_ = function(options) {
   var buttons = document.createElement('div');
   buttons.id = 'feedbackButtons';
+  buttons.style.width = '75%'; // sq
 
   let tryAgainText = undefined;
   if (!options.hideTryAgain) {
