122e2e19329eb7012bd0612f487dd9d13def2817
Chandresh <chandresh.singh@stayqrious.com>
2022-06-02 18:30:53 +0530

    sentry fix and version upgrade

--- apps/src/code-studio/initApp/sq.js
+++ apps/src/code-studio/initApp/sq.js
@@ -1,7 +1,8 @@
 import * as Sentry from '@sentry/browser';
+import { BrowserTracing } from "@sentry/tracing";
 
 
-Sentry.init({ dsn: 'https://93ee53fab0ef471d8da898849309536e@o353341.ingest.sentry.io/5286225', environment: process.env.NODE_ENV });
+Sentry.init({ dsn: 'https://93ee53fab0ef471d8da898849309536e@o353341.ingest.sentry.io/5286225', integrations: [new BrowserTracing()], environment: process.env.NODE_ENV });
 
 /*** Canvas patching ***/
 const getCanvasContext = HTMLCanvasElement.prototype.getContext;
